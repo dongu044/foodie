487d7c7ef403c0a9431aec7dd61150635946df9d
dongwoo-home <ddokkochi@gmail.com>
2025-09-01 22:20:27 +0900

    [fix] (user/signup): 컨트롤러 클래스명 변경
    상세 설명:
    - MemberController -> UserController

--- src/main/java/main/foodie/user/controller/UserController.java
+++ src/main/java/main/foodie/user/controller/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-public class MemberController {
+public class UserController {
 
   private final UserService userService;
 
